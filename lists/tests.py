8ac51cfe4def03430d3f35139f2d0a290b1774b6
Chuan Qin <qc2105@qq.com>
2018-04-11 10:13:58 +0800

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -29,6 +29,10 @@ class HomePageTest(TestCase):
     
 
 class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
