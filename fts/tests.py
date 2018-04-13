2eb92089b2b963a7a61d72f319c6f682ba7701cf
Chuan Qin <qc2105@qq.com>
2018-04-13 17:07:33 +0800

    different urls for different lists

--- fts/tests.py
+++ fts/tests.py
@@ -49,8 +49,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
    
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -98,10 +98,6 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
         
-
-
-        self.fail('Finish the test!')
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
