b97e0bd92367fc338d79b8da7ce7ce1a645405be
Chuan Qin <qc2105@qq.com>
2018-05-02 17:08:01 +0800

    Adjust new list view to do model validation

--- fts/test_list_item_validation.py
+++ fts/test_list_item_validation.py
@@ -21,14 +21,14 @@ class ItemValidationTest(FunctionalTest):
     
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys,ENTER)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
  
         # Perversely, she now decides to submit a second black list item
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
     
         # She receives a similar warning on the list page
-        self.wait_for(lambda: self.assertEuqal(
+        self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
